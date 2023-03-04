17c7493b49c10d8e67723f6cf5517a0878d4127d
Adrian Cillian Ansel <126966433+AdrianCillian@users.noreply.github.com>
2023-03-04 16:52:31 +0200

    Update heap.cpp

--- src/dev/heap.cpp
+++ src/dev/heap.cpp
@@ -8,100 +8,100 @@ extern "C" {
 
   // Define the cslua functions
   __declspec(dllexport) int cslua_open(lua_State* L) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_close(lua_State* L) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_dostring(lua_State* L, const char* str) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_dofile(lua_State* L, const char* filename) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_newstate(lua_State* L) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_load(lua_State* L, const char* chunk, size_t len, const char* name) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_dump(lua_State* L, lua_Writer writer, void* data) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) void cslua_createtable(lua_State* L, int narr, int nrec) {
-    // Function body here
+    
   }
 
   __declspec(dllexport) void cslua_newtable(lua_State* L) {
-    // Function body here
+    
   }
 
   __declspec(dllexport) void cslua_settable(lua_State* L, int idx) {
-    // Function body here
+    
   }
 
   __declspec(dllexport) void cslua_gettable(lua_State* L, int idx) {
-    // Function body here
+    
   }
 
   __declspec(dllexport) void cslua_setglobal(lua_State* L, const char* name) {
-    // Function body here
+    
   }
 
   __declspec(dllexport) void cslua_getglobal(lua_State* L, const char* name) {
-    // Function body here
+    
   }
 
   __declspec(dllexport) int cslua_type(lua_State* L, int idx) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_isnumber(lua_State* L, int idx) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_isstring(lua_State* L, int idx) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_iscfunction(lua_State* L, int idx) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_isuserdata(lua_State* L, int idx) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_isnil(lua_State* L, int idx) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_isboolean(lua_State* L, int idx) {
-    // Function body here
+    
     return 0;
   }
 
   __declspec(dllexport) int cslua_istable(lua_State* L, int idx) {
-    // Function body here
+    
     return 0;
   }
